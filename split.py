cb6db662094c40c739c6e99770b8143de97d4fa7
Siddhant <siddhantsharma669@gmail.com>
2021-10-25 12:05:43 +0530

    Python String split() Method

---
+++ split.py
@@ -0,0 +1,25 @@
+txt = "welcome to the jungle"
+
+x = txt.split()
+
+print(x)
+
+# output
+['welcome', 'to', 'the', 'jungle']
+
+
+
+
+
+
+txt = "apple#banana#cherry#orange"
+
+x = txt.split("#")
+
+print(x)
+
+# output
+['apple', 'banana', 'cherry', 'orange']
+
+# Syntax
+# string.split(separator, maxsplit)
